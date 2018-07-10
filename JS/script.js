e7486ea71a915ecb99c7404381be35fb77d67dc3
belkhiritaha <40890187+belkhiritaha@users.noreply.github.com>
2018-07-10 16:43:13 +0100

    bugged js efsdjkwcneiosdfwcjnkqidowcx
    not working pls halp me

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+/*---------------------------------------------------
+function myfunction() {
+	var img = document.getElementById("img1");
+    img.src="img4.jpg";
+    img.classList.add('opac');
+}
+
+function myfunction1() {
+	var img = document.getElementById("img1");
+    img.src="gal2.jpg";
+    img.classList.remove('opac');
+}
+---------------------------------------------------*/
+
+function myfunction3() {
+	setTimeout(3000);
+	var img = document.getElementById("img1");
+	img.classList.add('opac');
+	console.log('eosdj');
+	
+}
+
